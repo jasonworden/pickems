31d6d8e746c8cf8d3f0fb09c142c86a1c0a5a3ae
Jason Worden <wordenjasonp@gmail.com>
2017-01-17 17:06:04 -0800

    Hacked basic proof-of-concept PicksHome at the /picks route

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import { CounterButton, GithubButton } from 'components';
+import { CounterButton } from 'components';
 import config from '../../config';
 import Helmet from 'react-helmet';
 
@@ -20,31 +20,7 @@ export default class Home extends Component {
               </p>
             </div>
             <h1>{config.app.title}</h1>
-
             <h2>{config.app.description}</h2>
-
-            <p>
-              <a className={styles.github} href="https://github.com/erikras/react-redux-universal-hot-example"
-                 target="_blank">
-                <i className="fa fa-github"/> View on Github
-              </a>
-            </p>
-            <GithubButton user="erikras"
-                          repo="react-redux-universal-hot-example"
-                          type="star"
-                          width={160}
-                          height={30}
-                          count large/>
-            <GithubButton user="erikras"
-                          repo="react-redux-universal-hot-example"
-                          type="fork"
-                          width={160}
-                          height={30}
-                          count large/>
-
-            <p className={styles.humility}>
-              Created and maintained by <a href="https://twitter.com/erikras" target="_blank">@erikras</a>.
-            </p>
           </div>
         </div>
 
@@ -163,19 +139,6 @@ export default class Home extends Component {
             </dd>
           </dl>
 
-          <h3>From the author</h3>
-
-          <p>
-            I cobbled this together from a wide variety of similar "starter" repositories. As I post this in June 2015,
-            all of these libraries are right at the bleeding edge of web development. They may fall out of fashion as
-            quickly as they have come into it, but I personally believe that this stack is the future of web development
-            and will survive for several years. I'm building my new projects like this, and I recommend that you do,
-            too.
-          </p>
-
-          <p>Thanks for taking the time to check this out.</p>
-
-          <p>– Erik Rasmussen</p>
         </div>
       </div>
     );
