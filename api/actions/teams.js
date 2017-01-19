d8b9c20d8baec44cd613aa45a627ee8c1de7fe6c
Jason Worden <wordenjasonp@gmail.com>
2017-01-18 17:58:42 -0800

    Updated util method name

--- api/actions/teams.js
+++ api/actions/teams.js
@@ -1,10 +1,10 @@
 import NFLTeam from '../models/NFLTeam';
-import {createKeyedObjectFromArray} from '../utils/misc';
+import {createObjectFromArrayOfObjects} from '../utils/misc';
 
 export default function teams() {
   return new Promise((resolve) => {
     NFLTeam.find({}, (err, teams) => {
-      let teamsObject = createKeyedObjectFromArray(teams, 'abbreviation');
+      let teamsObject = createObjectFromArrayOfObjects(teams, 'abbreviation');
       resolve(teamsObject);
     });
   });
