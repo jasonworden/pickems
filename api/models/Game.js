74c330fc063d900bcb8f9a7f59c01daeb8db65c1
Jason Worden <wordenjasonp@gmail.com>
2017-01-23 20:30:24 -0800

    Added props onto Game model

--- api/models/Game.js
+++ api/models/Game.js
@@ -9,6 +9,10 @@ const schema = new Schema({
     type: Schema.Types.ObjectId,
     ref: 'Team'
   },
+  winner: {
+    type: Schema.Types.ObjectId,
+    ref: 'Team'
+  },
   week: {
     type: Schema.Types.ObjectId,
     ref: 'Week'
@@ -17,7 +21,15 @@ const schema = new Schema({
     type: Schema.Types.ObjectId,
     ref: 'NFLSeason'
   },
-  startTime: Date
+  startTime: Date,
+  isDecided: {
+    type: Boolean,
+    default: False
+  },
+  hadOvertime: {
+    type: Boolean,
+    defalt: False
+  },
 });
 
 const Game = mongoose.model('Game', schema);
