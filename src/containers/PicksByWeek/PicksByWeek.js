dbd2bab9af68ad1af62f9a05262308e44aad9d25
Jason Worden <wordenjasonp@gmail.com>
2017-01-26 15:57:56 -0800

    In PicksByWeek, show week 17 with note about not having picks. Also show if picks are locked.

--- src/containers/PicksByWeek/PicksByWeek.js
+++ src/containers/PicksByWeek/PicksByWeek.js
@@ -1,16 +1,19 @@
 import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
 import Label from 'react-bootstrap/lib/Label';
+import Badge from 'react-bootstrap/lib/Badge';
 import _ from 'lodash';
 
 @connect(
   state => ({
-    picks: state.picks.weeks
+    picks: state.picks.weeks,
+    weeks: state.schedule.weeks,
   })
 )
 export default class PicksByWeek extends Component {
   static propTypes = {
-    picks: PropTypes.object.isRequired
+    picks: PropTypes.object.isRequired,
+    weeks: PropTypes.object.isRequired,
   }
 
   constructor(props) {
@@ -18,13 +21,28 @@ export default class PicksByWeek extends Component {
     this.renderWeek = this.renderWeek.bind(this);
   }
 
-  renderWeek(weekNumber, picks) {
+  renderWeek(week, picks) {
+    const key = week._id;
+    if (!week.arePicksAllowed) {
+      return (
+        <li key={key}>
+          {week.number}.{' '}
+          <Label>NO PICKS THIS WEEK</Label>
+        </li>
+      );
+    }
+
     return (
-      <li key={weekNumber}>
+      <li key={key}>
         <h4>
-          {weekNumber}.{' '}
+          {week.number}.{' '}
           {picks.map(abbrev => (
-            <Label style={{marginRight: 5}}>{abbrev}</Label>
+            <Label style={{marginRight: 5}}>
+              {week.isLocked &&
+              <Badge><i className="fa fa-lock" /></Badge>
+              }
+              {abbrev}
+            </Label>
           ))}
         </h4>
       </li>
@@ -34,10 +52,16 @@ export default class PicksByWeek extends Component {
   render() {
     const style = require('./PicksByWeek.scss');
     const teams = [];
-    _.forEach(this.props.picks, (picks, weekNum) => {
-      teams.push(this.renderWeek(weekNum, picks));
+    _.forEach(this.props.weeks, week => {
+      const picks = this.props.picks[week.number];
+      teams.push(this.renderWeek(week, picks));
     });
 
-    return <ul className={style.teamsList + ' list-unstyled'}>{teams}</ul>;
+    return (
+      <div className="picks-by-week">
+        <h5 style={{fontWeight: 'bold', textTransform: 'uppercase'}}>Your picks by week</h5>
+        <ul className={style.teamsList + ' list-unstyled'}>{teams}</ul>
+      </div>
+    );
   }
 }
