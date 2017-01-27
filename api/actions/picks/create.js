3b5bfe77fca85f4822ffd57a510dc6218d7ce9b6
Jason Worden <wordenjasonp@gmail.com>
2017-01-27 01:22:10 -0800

    API endpoint for /picks/create now creates a pick

---
+++ api/actions/picks/create.js
@@ -0,0 +1,27 @@
+import Pick from '../../models/Pick';
+
+// Currently creates a pick that is sent from the client,
+// using client-side validation and no server-side validation.
+// This is bad, but okay for our dummy app :)
+// TODO: Use robust server-side validation on making picks.
+
+export default function create(req) {
+  // TODO: pull user off the session, not from the posted data
+  return new Promise((resolve, reject) => {
+    if (!req.body.user) {
+      reject({ error: "Not logged in" });
+      return;
+    }
+    const pick = new Pick(req.body);
+    pick.save(function(err, savedPick) {
+      if (err) {
+        reject({ error: "Unable to create pick" });
+        return;
+      }
+      savedPick.populate('nflteam week', (err, pick) => {
+        resolve(pick.toObject());
+        return;
+      });
+    });
+  });
+}
