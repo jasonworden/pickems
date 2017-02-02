1be3d4d697bb7cb6935404bd368b2aec14a132a0
Jason Worden <wordenjasonp@gmail.com>
2017-02-01 19:33:46 -0800

    Replace 404 error with corny incomplete pass copy

--- src/containers/NotFound/NotFound.js
+++ src/containers/NotFound/NotFound.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function NotFound() {
   return (
     <div className="container">
-      <h1>Doh! 404!</h1>
-      <p>These are <em>not</em> the droids you are looking for!</p>
+      <h1>Incomplete pass!</h1>
+      <p>No one was there to catch that pass. Did you enter the correct URL?</p>
     </div>
   );
 }
